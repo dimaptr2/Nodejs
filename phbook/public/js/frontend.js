6f9f6f39aa28511ce9075c3ca748e85b9c2e3ccf
dpetrov <dpetrov@fclient.org>
2017-03-25 13:23:48 +0300

    PhoneBook

--- phbook/public/js/frontend.js
+++ phbook/public/js/frontend.js
@@ -1,9 +1,7 @@
 $(function() {
 
-    // click in the create button
-    $("#create").click(function(event) {
-        // clear old elements
-       var bra = "<div id=\"active-area\" class=\"dynamic-area\">\n";
+     var window1 = "";
+     var bra = "<div id=\"active-area\" class=\"dynamic-area\">\n";
        var label1 = "<label class=\"label1\">Имя: </label><br>";
        var fname = "<input id=\"firstName\" type=\"text\" class=\"input1 input2\"><br>";
        var label2 = "<label class=\"label1\">Фамилия: </label><br>";
@@ -14,25 +12,61 @@ $(function() {
        var phone = "<input id=\"phone\" type=\"text\" class=\"input1 input2\""
        + " placeholder=\"+7(___) ___-__-__\">";
        var label5 = "<label class=\"label1\"> Вн. номер: </label>";
-       var ext = "<input id=\"extension\" type=\"text\" class=\"mini-input1 mini-input2\"><br>";
+       var ext = "<input id=\"extension\" type=\"text\" class=\"input1 input2 mini1 mini2\"><br>";
        var label6 = "<label class=\"label1\">E-mail: </label><br>";
+       var email = "<input id=\"email\" type=\"text\" class=\"input1 input2\"><br>";
 
-       var elem = bra;
-       elem = elem + label1 + "\n" + fname + "\n" + label2 + "\n" + lname + "\n" + label3 + "\n" + sname;
-       elem = elem + "\n" + label4 + "\n" + phone + "\n" + label5 + "\n" + ext;
-       elem = elem + "\n" + label6;
-       var ket = "</div>";
-       elem = elem + ket;
+       var window1 = bra;
+       window1 = window1 + label1 + "\n" + fname + "\n" + label2 + "\n" + lname + "\n" + label3 + "\n" + sname;
+       window1 = window1 + "\n" + label4 + "\n" + phone + "\n" + label5 + "\n" + ext;
+       window1 = window1 + "\n" + label6 + "\n" + email;
+       var ket = "\n" + "</div>";
+       window1 = window1 + ket;
 
-       $("#middle-area").html(elem);
+    var window2 = "<div id=\"active-area\" class=\"dynamic-area\">\n";
+        window2 = window2 + "<label class=\"label1\">Найти сорудника?</label><br>\n";
+        window2 = window2 + "<input id=\"regex\" type=\"text\" class=\"input1 input2 maxi1 maxi2\">\n";
+        window2 = window2 + "\n</div>";
 
+    var buttonSearch = "<button id=\"btnSearch\" class=\"button1 button2\">Поиск</button>";
+
+    // click in the create button
+    $("#create").click(function(event) {
+       
+       deleteActiveArea();
+        // clear old elements
+        addActiveArea(window1);
     });
 
     // Change the data on the screen
     $("#change").click(function(event) {
+        deleteActiveArea();
+        addActiveArea(window2);
+        $("#left-area").append(buttonSearch);
+    });  // change
 
-        $("#active-area").remove();
-
+    $("#btnSearch").click(function(event) {
+            alert("Поиск");
     });
 
+    $("#read").click(function(event) {
+        deleteActiveArea();
+        addActiveArea(window2);
+        $("#left-area").append(buttonSearch);
+    }); // read
+
+    $("#delete").click(function(event) {
+        deleteActiveArea();
+    }); // delete
+
+
+// Here are additional functions
+    function addActiveArea(win) { 
+        $("#middle-area").html(win);
+    }
+
+    function deleteActiveArea() {
+        $("#active-area").remove();
+        $("#btnSearch").remove();
+    }
 }); // jquery using
