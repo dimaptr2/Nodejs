4bb402d20c057391114bea6ae468b550515e9f99
dpetrov <dpetrov@fclient.org>
2017-03-26 12:17:12 +0300

    Phbook version 4

--- phbook/main.js
+++ phbook/main.js
@@ -1,8 +1,12 @@
+const PORT = 8080;
+const GLPATH = '/usr/local/lib/node_modules';
+
 var path = require('path');
 var express = require('express');
+
 var app = express();
-const PORT = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
-console.log("Start Node Server on the port: " + PORT);
+
+console.log('Start Node Server on the port 8080');
 app.listen(PORT);
--- phbook/public/js/frontend.js
+++ phbook/public/js/frontend.js
@@ -16,57 +16,66 @@ $(function() {
        var label6 = "<label class=\"label1\">E-mail: </label><br>";
        var email = "<input id=\"email\" type=\"text\" class=\"input1 input2\"><br>";
 
+       var buttonSave = "<button id=\"btnSave\" class=\"button1 button2\">Сохранить</button>";
+
        var window1 = bra;
        window1 = window1 + label1 + "\n" + fname + "\n" + label2 + "\n" + lname + "\n" + label3 + "\n" + sname;
        window1 = window1 + "\n" + label4 + "\n" + phone + "\n" + label5 + "\n" + ext;
-       window1 = window1 + "\n" + label6 + "\n" + email;
+       window1 = window1 + "\n" + label6 + "\n" + email + "\n" + buttonSave;
        var ket = "\n" + "</div>";
        window1 = window1 + ket;
 
+    var buttonSearch = "<button id=\"btnSearch\" class=\"button1 button2\">Поиск</button>";
+
     var window2 = "<div id=\"active-area\" class=\"dynamic-area\">\n";
         window2 = window2 + "<label class=\"label1\">Найти сорудника?</label><br>\n";
-        window2 = window2 + "<input id=\"regex\" type=\"text\" class=\"input1 input2 maxi1 maxi2\">\n";
+        window2 = window2 + "<input id=\"mask\" type=\"text\" name=\"mask\" class=\"input1 input2 maxi1 maxi2\"><br>\n";
+        window2 = window2 + buttonSearch;
         window2 = window2 + "\n</div>";
-
-    var buttonSearch = "<button id=\"btnSearch\" class=\"button1 button2\">Поиск</button>";
+    
 
     // click in the create button
     $("#create").click(function(event) {
-       
-       deleteActiveArea();
-        // clear old elements
-        addActiveArea(window1);
+        $("#active-area").remove();
+        $("#middle-area").html(window1);
+        $("#btnSave").click(function(event) {
+
+        });
     });
 
     // Change the data on the screen
     $("#change").click(function(event) {
-        deleteActiveArea();
-        addActiveArea(window2);
-        $("#left-area").append(buttonSearch);
+        $("#active-area").remove();
+        $("#middle-area").html(window2);
+        // search emplyees and add a button 
+        $("#btnSearch").click(function(event) {
+            var mask = $("#mask").val();
+            $.post("/book", {mask: mask}, function(data) {
+                var keys = ["mask"];
+                var result = JSON.stringify(data, keys);
+                $("#answer").remove();
+                var answer = JSON.parse(result);
+                $("#active-area").append("<p id=\"answer\" class=\"signum-style\">" + answer + "</p>");
+            }); // post
+        }); // button search event
+
     });  // change
 
-    $("#btnSearch").click(function(event) {
-            alert("Поиск");
-    });
-
     $("#read").click(function(event) {
-        deleteActiveArea();
-        addActiveArea(window2);
-        $("#left-area").append(buttonSearch);
+        $("#active-area").remove();
+        $("#middle-area").html(window2);
+        // search emploees
+        $("#btnSearch").click(function(event) {
+            var value = $("#mask").val();
+            $("#answer").remove();
+            $("#active-area").append("<p id=\"answer\" class=\"signum-style\">" + value + "</p>");
+        });
     }); // read
 
-    $("#delete").click(function(event) {
-        deleteActiveArea();
+    $("#clean").click(function(event) {
+        $("#active-area").remove();
     }); // delete
 
+   
 
-// Here are additional functions
-    function addActiveArea(win) { 
-        $("#middle-area").html(win);
-    }
-
-    function deleteActiveArea() {
-        $("#active-area").remove();
-        $("#btnSearch").remove();
-    }
 }); // jquery using
